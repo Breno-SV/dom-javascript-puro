52b3ca3d5a53e00c5c6cdcec94f24bff8dbc4044
Breno-SV <brenozildox@gmail.com>
2026-01-06 15:58:39 -0300

    Funcionalidade do som

--- js/script.js
+++ js/script.js
@@ -18,19 +18,34 @@ const charmander = document.querySelector("#charmanderr");
 const pikachu = document.querySelector("#pikachuu");
 const zubat = document.querySelector("#zubatt");
 
+const audio = document.querySelector("audio");
+audio.volume = 0.1;
+
+const musicControl = document.querySelector(".music-control");
+musicControl.addEventListener('click', (Event) => {
+    Event.stopPropagation();
+
+    Event.target.src = `${Event.target.src}`.includes("onn.png") 
+    ? "../assets/icons/mute.png"
+    : "../assets/icons/onn.png";
+    
+    `${Event.target.src}`.includes("onn.png") ? audio.play() : audio.pause();
+});
+
 // também poderia usar a function dessa forma:
 // function teste() {}
 // body.addEventListener("keydown", teste);
 
 function getRightPosition() {
-    console.log()
-    return position = parseInt(ash.style.right.split("px")) || 2;
+    //console.log()
+    return parseInt(ash.style.right.split("px")) || 2;
 
-    console.log(position);
+    //console.log(position);
 }
 
 function getTopPosition() {
-    console.log(ash.style.top);
+    //console.log(ash.style.top);
+    return parseInt(ash.style.top.split("px")) || 2;
 }
 
 body.addEventListener("keydown", (Event) => {
@@ -46,15 +61,24 @@ body.addEventListener("keydown", (Event) => {
             break;
 
         case "ArrowRight":
+            if(getRightPosition() > 2){
+                ash.style.right = `${getRightPosition() - 8}px`;    
                 ash.src = "../assets/rightt.png";
+            }
             break;
 
         case "ArrowDown":
-            ash.src = "../assets/frontt.png";
+            if(getTopPosition() < 625){
+                ash.style.top = `${getTopPosition() + 8}px`;    
+                ash.src = "../assets/frontt.png";
+            }
             break;
 
         case "ArrowUp":
-            ash.src = "../assets/backk.png";
+            if(getTopPosition() > 2){
+                ash.style.top = `${getTopPosition() - 8}px`;    
+                ash.src = "../assets/backk.png";
+            }
             break;
 
         default:
